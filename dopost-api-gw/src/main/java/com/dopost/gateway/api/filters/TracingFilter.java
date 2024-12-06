842148ecb245ed31f0020df59d82bc353b5febf3
Leandro Blandi <leandronahuelblandiomg@gmail.com>
2024-12-06 15:53:25 -0300

    refactor(gw): changed log implementation

--- dopost-api-gw/src/main/java/com/dopost/gateway/api/filters/TracingFilter.java
+++ dopost-api-gw/src/main/java/com/dopost/gateway/api/filters/TracingFilter.java
@@ -3,20 +3,18 @@ package com.dopost.gateway.api.filters;
 import java.time.Duration;
 import java.time.Instant;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Component
 public class TracingFilter implements GlobalFilter, Ordered {
-
-    private static final Logger logger = LoggerFactory.getLogger(TracingFilter.class);
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
@@ -24,7 +22,7 @@ public class TracingFilter implements GlobalFilter, Ordered {
         String requestPath = exchange.getRequest().getURI().getPath();
         Instant startTime = Instant.now();
 
-        logger.info("Request ID: {} - Incoming request to {} at {}", requestId, requestPath, startTime);
+        log.info("Request ID: {} - Incoming request to {} at {}", requestId, requestPath, startTime);
 
         exchange.getRequest().mutate()
             .header("X-Request-Id", requestId)
@@ -33,7 +31,7 @@ public class TracingFilter implements GlobalFilter, Ordered {
         return chain.filter(exchange)
             .doOnTerminate(() -> {
                 Instant endTime = Instant.now();
-                logger.info("Request ID: {} - Completed request to {} in {} ms", 
+                log.info("Request ID: {} - Completed request to {} in {} ms", 
                             requestId, requestPath, Duration.between(startTime, endTime).toMillis());
             });
     }
